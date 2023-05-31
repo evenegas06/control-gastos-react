88bb3e1fed80f00c8b0f300cf730dcb4732dac5c
Eduardo Venegas <evenegas06@hotmail.com>
2023-05-30 19:42:01 -0600

    form on modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -25,6 +25,43 @@ const Modal = ({ setModal, animation, setAnimation }) => {
 
             <form className={`formulario ${animation ? "animar" : "cerrar"}`}>
                 <legend>Nuevo gasto</legend>
+
+                <div className="campo">
+                    <label htmlFor="name">Nombre del gasto</label>
+
+                    <input
+                        id="name"
+                        type="text"
+                        placeholder="Añade el nombre de tu gasto"
+                    />
+                </div>
+
+                <div className="campo">
+                    <label htmlFor="amount">Cantidad</label>
+
+                    <input
+                        id="amount"
+                        type="number"
+                        placeholder="Añade la cantidad del gasto ej. 300"
+                    />
+                </div>
+
+                <div className="campo">
+                    <label htmlFor="category">Categoría</label>
+
+                    <select name="category" id="category">
+                        <option value="">-- Seleccione --</option>
+                        <option value="saving">Ahorro</option>
+                        <option value="food">Comida</option>
+                        <option value="home">Hogar</option>
+                        <option value="others">Gastos varios</option>
+                        <option value="entertainment">Entretenimiento</option>
+                        <option value="health">Salud</option>
+                        <option value="subscriptions">Suscripciones</option>
+                    </select>
+                </div>
+
+                <input type="submit" value="Añadir gasto" />
             </form>
         </div>
     );
